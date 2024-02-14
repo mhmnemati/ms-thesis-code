992e8b458248e7b509bedd901589f16e1fd8591c
KoLiBer <koliberr136a1@gmail.com>
2024-02-14 07:31:05 +0000

    fix: set default floating point to 16-bit

--- main.py
+++ main.py
@@ -5,18 +5,13 @@ import tensorflow_datasets as tfds
 from models import DeepSleepNet
 from callbacks import callbacks
 
+tf.keras.backend.set_floatx("float16")
 
 train_set, test_set = tfds.load("sleep_edfx", split=["train[:5%]", "test[:5%]"], as_supervised=True)
 
-
-def process(data, label):
-    return data, label
-
-
 AUTOTUNE = tf.data.AUTOTUNE
-
-train_set = train_set.map(process, num_parallel_calls=AUTOTUNE).cache().batch(8).prefetch(AUTOTUNE)
-test_set = test_set.map(process, num_parallel_calls=AUTOTUNE).cache().batch(8).prefetch(AUTOTUNE)
+train_set = train_set.cache().batch(16).prefetch(AUTOTUNE)
+test_set = test_set.cache().batch(16).prefetch(AUTOTUNE)
 
 model = DeepSleepNet(num_class=6)
 
@@ -28,8 +23,8 @@ model.compile(
 )
 model.fit(
     train_set, epochs=10,
-    batch_size=8,
+    batch_size=16,
     validation_data=test_set,
-    validation_batch_size=8,
+    validation_batch_size=16,
     callbacks=callbacks,
 )
