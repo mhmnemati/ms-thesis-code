4b3421e3d60fb0a21d94c1376867b57882129ba7
KoLiBer <koliberr136a1@gmail.com>
2024-02-24 16:05:44 +0330

    fix: remove batch_size in fit method

--- src/main.py
+++ src/main.py
@@ -4,15 +4,7 @@ import tensorflow_datasets as tfds
 from models import DeepSleepNet
 from callbacks import callbacks
 
-# try:
-#     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
-# except ValueError:
-#     tpu = None
-
-# if tpu:
-#     tf.keras.mixed_precision.set_global_policy("mixed_bfloat16")
-# else:
-#     tf.keras.mixed_precision.set_global_policy("mixed_float16")
+tf.keras.mixed_precision.set_global_policy("mixed_float16")
 
 train_set, test_set = tfds.load("sleep_edfx/20", split=["train", "test"], as_supervised=True)
 
@@ -29,10 +21,4 @@ model.compile(
     loss="sparse_categorical_crossentropy",
     metrics=["accuracy"]
 )
-model.fit(
-    train_set, epochs=10,
-    batch_size=BATCH,
-    validation_data=test_set,
-    validation_batch_size=BATCH,
-    callbacks=callbacks,
-)
+model.fit(train_set, epochs=10, validation_data=test_set, callbacks=callbacks)
