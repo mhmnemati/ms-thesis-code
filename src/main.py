fe501a0f77648454324d2274cf796844ee40281d
KoLiBer <koliberr136a1@gmail.com>
2024-04-12 14:50:53 +0000

    fix: change model name

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from models import Brain2Vec, EEGInception
 
 model = Brain2Vec()
 trainer = L.Trainer(max_epochs=5)
-trainer.fit(model, EEGInception.loader(CHBMITDataset, train=True), EEGInception.loader(CHBMITDataset, train=False))
+trainer.fit(model, Brain2Vec.loader(CHBMITDataset, train=True), Brain2Vec.loader(CHBMITDataset, train=False))
 
 # model = EEGInception()
 # trainer = L.Trainer(max_epochs=5)
