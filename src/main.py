d1a28ee01a99965b9e4b62dca7b19b2a32acfa95
KoLiBer <koliberr136a1@gmail.com>
2024-04-12 13:29:52 +0000

    feat: add Brain2Vec GNN based model

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import torch as T
 import lightning as L
 
 from data import CHBMITDataset
-from torch.utils.data import DataLoader
-from models import EEGInception
+from torch_geometric.data import Data
+from torch_geometric.loader import DataLoader
+from models import Brain2Vec
+
+from scipy import sparse
+from torch_geometric.utils.convert import from_scipy_sparse_matrix
 
 
 import numpy as np
@@ -15,7 +20,7 @@ def transform(item):
 
     electrodes = np.unique(np.concatenate([sources, targets]), axis=0)
 
-    node_features = np.zeros((electrodes.shape[0], data.shape[1]))
+    node_features = np.zeros((electrodes.shape[0], data.shape[1]), dtype=np.float32)
     for i in range(len(data)):
         power = data[i] ** 2
 
@@ -25,16 +30,15 @@ def transform(item):
         node_features[source_idx] += power / 2
         node_features[target_idx] += power / 2
 
-    adjecancy_matrix = np.zeros((electrodes.shape[0], electrodes.shape[0]))
+    adjecancy_matrix = np.zeros((electrodes.shape[0], electrodes.shape[0]), dtype=np.float64)
     for i in range(electrodes.shape[0]):
         for j in range(electrodes.shape[0]):
             distance = np.linalg.norm(electrodes[j] - electrodes[i])
             adjecancy_matrix[i, j] = 1 if distance < 0.1 else 0
 
-    print(node_features)
-    print(adjecancy_matrix)
+    edge_index = from_scipy_sparse_matrix(sparse.csr_matrix(adjecancy_matrix))[0]
 
-    return (item["data"], item["label"])
+    return Data(x=T.from_numpy(node_features), edge_index=edge_index, y=T.tensor([item["label"]], dtype=T.int64))
 
 
 BATCH = 8
@@ -45,8 +49,6 @@ train_loader = DataLoader(train_set, batch_size=BATCH, num_workers=4)
 test_set = CHBMITDataset(train=False, transform=transform)
 test_loader = DataLoader(test_set, batch_size=BATCH, num_workers=4)
 
-train_set[5]
-
-# model = EEGInception()
-# trainer = L.Trainer(max_epochs=5)
-# trainer.fit(model, train_loader, test_loader)
+model = Brain2Vec()
+trainer = L.Trainer(max_epochs=5)
+trainer.fit(model, train_loader, test_loader)
--- src/models/brain2vec.py
+++ src/models/brain2vec.py
@@ -2,8 +2,57 @@ import torch as T
 import lightning as L
 import torchmetrics as M
 import torch.nn.functional as F
-from braindecode.models import EEGInception as Model
+
+import torch.nn as tnn
+import torch_geometric.nn as gnn
 
 
 class Brain2Vec(L.LightningModule):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.conv1 = gnn.GCNConv(in_channels=3000, out_channels=2000)
+        self.conv2 = gnn.GCNConv(in_channels=2000, out_channels=1000)
+        self.linear = tnn.Linear(in_features=1000, out_features=2)
+
+        self.training_accuracy = M.Accuracy(task="multiclass", num_classes=2)
+        self.validation_accuracy = M.Accuracy(task="multiclass", num_classes=2)
+
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = gnn.global_mean_pool(x, batch)
+        x = F.dropout(x, p=0.5)
+        x = self.linear(x)
+        x = F.softmax(x)
+        return x
+
+    def configure_optimizers(self):
+        return T.optim.SGD(self.parameters(), lr=1e-3)
+
+    def training_step(self, batch, idx):
+        x = self.conv1(batch.x, batch.edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, batch.edge_index)
+        x = gnn.global_mean_pool(x, batch.batch)
+        x = F.dropout(x, p=0.5)
+        x = self.linear(x)
+        pred = F.softmax(x)
+
+        loss = F.cross_entropy(pred, batch.y)
+        self.log("training_loss", loss)
+        self.log("training_accuracy", self.training_accuracy(pred, batch.y))
+        return loss
+
+    def validation_step(self, batch, idx):
+        x = self.conv1(batch.x, batch.edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, batch.edge_index)
+        x = gnn.global_mean_pool(x, batch.batch)
+        x = F.dropout(x, p=0.5)
+        x = self.linear(x)
+        pred = F.softmax(x)
+
+        loss = F.cross_entropy(pred, batch.y)
+        self.log("validation_loss", loss)
+        self.log("validation_accuracy", self.validation_accuracy(pred, batch.y))
