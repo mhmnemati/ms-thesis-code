fd697dc029f3f86c4af3cd72a7c9ae8a48572a3e
KoLiBer <koliberr136a1@gmail.com>
2024-04-24 21:48:47 +0000

    clean: remove callbacks

--- src/callbacks/__init__.py
+++
@@ -1,8 +0,0 @@
-import keras
-
-callbacks = [
-    keras.callbacks.BackupAndRestore(backup_dir="./backup"),
-    keras.callbacks.ModelCheckpoint(filepath="./ckpt"),
-    keras.callbacks.TensorBoard(log_dir="./logs"),
-    # keras.callbacks.ProgbarLogger()
-]
