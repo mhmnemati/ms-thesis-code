424463f53c9e714aeaefac824be325d6a89b45d4
KoLiBer <koliberr136a1@gmail.com>
2024-05-20 13:50:54 +0000

    fix: refactor chb_mit generator

--- src/generators/chb_mit.py
+++ src/generators/chb_mit.py
@@ -72,20 +72,6 @@ class Generator:
 
             normals = normals[selects]
 
-            normal_selections = {}
-            for idx, label_idx in normals:
-                if idx not in normal_selections:
-                    normal_selections[idx] = []
-
-                normal_selections[idx].append(label_idx)
-
-            seizure_selections = {}
-            for idx, label_idx in seizures:
-                if idx not in seizure_selections:
-                    seizure_selections[idx] = []
-
-                seizure_selections[idx].append(label_idx)
-
             for idx, record in enumerate(records):
                 raw = mne.io.read_raw_edf(record, infer_types=True, include=[
                     "FP1-F7", "F7-T7", "T7-P7", "P7-O1", "FP1-F3", "F3-C3", "C3-P3", "P3-O1",
@@ -100,7 +86,8 @@ class Generator:
 
                 data = raw.get_data(tmin=tmin, tmax=tmax, picks=picks).astype(np.float32)
 
-                for low in normal_selections[idx]:
+                seconds = np.concatenate([normals[normals[:, 0] == idx][:, 1], seizures[seizures[:, 0] == idx][:, 1]])
+                for low in seconds:
                     high = low + self.window
                     sfreq = raw.info["sfreq"]
 
@@ -112,20 +99,6 @@ class Generator:
                         "sources": sources,
                         "targets": targets,
                     }
-
-                if idx in seizure_selections:
-                    for low in seizure_selections[idx]:
-                        high = low + self.window
-                        sfreq = raw.info["sfreq"]
-
-                        _labels = labels[int(low):int(high)]
-
-                        yield f"{patient}/{self.labels[_labels.max()]}", {
-                            "data": data[:, int(low*sfreq):int(high*sfreq)],
-                            "labels": _labels,
-                            "sources": sources,
-                            "targets": targets,
-                        }
 
     def get_labels(self, raw, annotations):
         seconds = int(raw.n_times / raw.info["sfreq"])
@@ -145,13 +118,13 @@ class Generator:
     def get_montage(self, raw, positions):
         picks = mne.pick_types(raw.info, eeg=True)
 
-        sources = []
-        targets = []
+        sources = np.zeros((len(picks), 3), dtype=np.float32)
+        targets = np.zeros((len(picks), 3), dtype=np.float32)
         for idx, pick in enumerate(picks):
             channel = raw.info["ch_names"][pick]
             electrodes = channel.upper().split("-")
-            sources.append(electrodes[0])
-            targets.append(electrodes[1])
+            sources[idx] = positions[electrodes[0]]
+            targets[idx] = positions[electrodes[1]]
 
         return sources, targets, picks
 
