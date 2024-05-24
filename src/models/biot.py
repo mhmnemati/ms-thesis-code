fix: add normalization layer

src/models/biot.py
@@ -171,13 +171,11 @@ class BIOT(BaseModel):
             loss=pt.nn.CrossEntropyLoss()
         )
 
-    def configure_optimizers(self):
-        optimizer = pt.optim.Adam(self.parameters(), lr=1e-2, weight_decay=1e-5)
-        scheduler = pt.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[int(1 * 100), int(4 * 100)], gamma=0.1)
-
-        return [optimizer], [scheduler]
-
     def transform(self, item):
+        for i in range(item["data"].shape[0]):
+            percentile_95 = np.percentile(np.abs(item["data"][i]), 95, axis=0, keepdims=True)
+            item["data"][i] = item["data"][i] / percentile_95
+
         channels = [
             "FP1-F7",
             "F7-T7",
@@ -200,28 +198,16 @@ class BIOT(BaseModel):
         ]
 
         data = np.zeros((len(channels), 30 * 200), dtype=np.float32)
-
         for idx, ch_name in enumerate(item["ch_names"]):
-            if "EEG" not in ch_name:
-                continue
-
-            if ch_name.replace("EEG ", "") == "Fpz-Cz":
-                signal = item["data"][idx]
-                percentile_95 = np.percentile(np.abs(signal), 95, axis=0, keepdims=True)
-                signal = signal / percentile_95
-
-                signal = sp.signal.resample(signal, 30 * 200) / 2
+            if ch_name == "EEG Fpz-Cz":
+                signal = sp.signal.resample(item["data"][idx], 30 * 200) / 2
                 data[channels.index("FP1-F3")] = signal
                 data[channels.index("F3-C3")] = signal
                 data[channels.index("FP2-F4")] = signal
                 data[channels.index("F4-C4")] = signal
 
-            if ch_name.replace("EEG ", "") == "Pz-Oz":
-                signal = item["data"][idx]
-                percentile_95 = np.percentile(np.abs(signal), 95, axis=0, keepdims=True)
-                signal = signal / percentile_95
-
-                signal = sp.signal.resample(signal, 30 * 200)
+            if ch_name == "EEG Pz-Oz":
+                signal = sp.signal.resample(item["data"][idx], 30 * 200)
                 data[channels.index("P3-O1")] = signal
                 data[channels.index("P4-O2")] = signal
 

src/models/deep4net.py
@@ -19,13 +19,10 @@ class Deep4Net(BaseModel):
         )
 
     def transform(self, item):
-        data = item["data"]
-        label = item["labels"].max()
+        data = np.zeros((self.hparams.n_chans, item["data"].shape[1]))
+        data[:item["data"].shape[0]] = item["data"]
 
-        if data.shape[0] != self.hparams.n_chans:
-            data = np.concatenate([data, np.zeros((self.hparams.n_chans - data.shape[0], data.shape[1]))])
-
-        return data, label
+        return data, item["labels"].max()
 
     @staticmethod
     def add_arguments(parent_parser):

src/models/gcnattn.py
@@ -18,8 +18,6 @@ class Model(T.Module):
         super().__init__()
 
         self.model = G.Sequential("x, edge_index, batch", [
-            (T.InstanceNorm1d(num_features=int(n_times/1)), "x -> x"),
-
             (G.GCNConv(in_channels=int(n_times/1), out_channels=int(n_times/2)), "x, edge_index -> x"),
             (T.BatchNorm1d(num_features=int(n_times/2)), "x -> x"),
             (T.ReLU(), "x -> x"),
@@ -55,23 +53,24 @@ class GCNAttn(BaseModel):
         )
 
     def transform(self, item):
-        data = item["data"]
-        labels = item["labels"]
+        for i in range(item["data"].shape[0]):
+            percentile_95 = np.percentile(np.abs(item["data"][i]), 95, axis=0, keepdims=True)
+            item["data"][i] = item["data"][i] / percentile_95
 
-        node_features = np.zeros((data.shape[0], data.shape[1]), dtype=np.float32)
-        for i in range(data.shape[0]):
-            node_features[i] = np.abs(np.fft.fft(data[i, :]))
+        node_features = np.zeros((item["data"].shape[0], item["data"].shape[1]), dtype=np.float32)
+        for i in range(item["data"].shape[0]):
+            node_features[i] = np.abs(np.fft.fft(item["data"][i]))
 
-        adjecancy_matrix = np.zeros((data.shape[0], data.shape[0]))
-        for i in range(data.shape[0]):
-            for j in range(data.shape[0]):
-                correlation = sp.stats.pearsonr(data[i], data[j]).statistic
+        adjecancy_matrix = np.zeros((node_features.shape[0], node_features.shape[0]))
+        for i in range(adjecancy_matrix.shape[0]):
+            for j in range(adjecancy_matrix.shape[1]):
+                correlation = sp.stats.pearsonr(item["data"][i], item["data"][j]).statistic
                 if correlation > 0.3:
                     adjecancy_matrix[i, j] = 1
 
         return Data(
             x=pt.from_numpy(node_features),
-            y=pt.tensor(labels.max()),
+            y=pt.tensor(item["labels"].max()),
             edge_index=from_scipy_sparse_matrix(sp.sparse.csr_matrix(adjecancy_matrix))[0],
         )
 